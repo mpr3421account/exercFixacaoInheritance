dc7512995f3cbdce956b06f865a722837ee5e171
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2023-01-04 11:58:31 -0300

    fim com erro no string final

--- Program.cs
+++ Program.cs
@@ -11,4 +11,45 @@ exemplo (próxima página). Para produtos
 importados, a taxa e alfândega deve ser
 acrescentada ao preço final do produto.
 */
+using exercFixacaoInheritance.Entities;
+using System.Globalization;
+using System.Net.Http.Headers;
 
+
+Console.Write("Enter the number of products: ");
+int n = int.Parse(Console.ReadLine());
+
+List<Product> list = new List<Product>();
+
+for(int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"Product #{i} data:");
+    Console.Write("Common, used or imported (c/u/i)? ");
+    char c = char.Parse(Console.ReadLine());
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Price: ");
+    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    if(c == 'i')
+    {
+        Console.Write("Custom fee: ");
+        double customFee = double.Parse(Console.ReadLine());
+        list.Add(new ImportedProduct(name,price, customFee));
+    }
+    else if(c == 'u')
+    {
+        Console.Write("Manufacture date (DD/MM/YYYY):");
+        DateTime date = DateTime.Parse(Console.ReadLine());
+        list.Add(new UsedProduct(name, price, date));
+    }
+    else
+    {
+        list.Add(new Product(name, price));
+    }
+}
+Console.WriteLine();
+Console.WriteLine("PRICE TAGS:");
+foreach(Product prod in list)
+{
+    Console.WriteLine(prod.PriceTag);
+}
